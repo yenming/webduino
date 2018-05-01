e9ebde6f98e968d7befbbe4d67c8f80673764a2b
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-01 15:12:59 +0800

    Add files via upload

---
+++ RGB_LED_20180219/rgb_led.js
@@ -0,0 +1,13 @@
++(function (window, document) {
+
+  'use strict';
+  
+  
+  function funRGB_LED(pin_red,value_red,pin_green,value_green,pin_blue,value_blue)
+  {
+    console.log(pin_red+','+value_red+','+pin_green+','+value_green+','+pin_blue+','+value_blue);
+  }
+    
+  window.funRGB_LED = funRGB_LED;
+  
+}(window, window.document));
