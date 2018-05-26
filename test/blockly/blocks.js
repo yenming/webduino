08bdd976ccf33305560e94f5cc31c4ca939df89c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-26 19:31:53 +0800

    Update blocks.js

--- test/blockly/blocks.js
+++ test/blockly/blocks.js
@@ -1,37 +1,7 @@
-Blockly.Blocks['iframe_open'] = {
-  init: function() {
-  this.appendValueInput("url_")
-      .setCheck("String")
-      .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.TEXT_SHOW)
-      .appendField(Blockly.Msg.URL_SHOW)
-  this.appendValueInput("width_")
-      .setCheck("Number")
-      .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.WIDTH_SHOW)
-  this.appendValueInput("height_")
-      .setCheck("Number")
-      .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.HEIGHT_SHOW);
-  this.appendValueInput("left_")
-      .setCheck("Number")
-      .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.LEFT_SHOW);    
-    this.appendValueInput("top_")
-      .setCheck("Number")
-      .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.TOP_SHOW);  
-  this.setPreviousStatement(true);
-  this.setNextStatement(true);
-  this.setTooltip('');
-  this.setColour(65);
-  this.setHelpUrl(mainUrl + 'basic/blockly/blockly-information.html' + utmUrl);
-  }
-};
-Blockly.Blocks['iframe_close'] = {
+Blockly.Blocks['create_button'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField(Blockly.Msg.CLOSE_SHOW)
+        .appendField(Blockly.Msg.create_button)
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(65);
