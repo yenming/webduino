d53ff2e10f610303490dd84e25d7f5862318420f
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-26 19:59:38 +0800

    Update createbutton.js

--- modifyWebDemo_20180526/createbutton.js
+++ modifyWebDemo_20180526/createbutton.js
@@ -15,9 +15,9 @@
         document.body.appendChild(div);
 
         var list = document.getElementsByClassName("db5");
-         for(var i = list.length - 1; 0 <= i; i--)
-         if(list[i] && list[i].parentElement)
-         list[i].parentElement.removeChild(list[i]);
+          for(var i = list.length - 1; 0 <= i; i--)
+          if(list[i] && list[i].parentElement)
+          list[i].parentElement.removeChild(list[i]);
 
         var divHtml="<table><tr><td></td><td><button id='demo-area-05-btn1' class='db5'>按鈕 1</button></td><td></td></tr><tr><td><button id='demo-area-05-btn2' class='db5'>按鈕 2</button></td><td><button id='demo-area-05-btn3' class='db5'>按鈕 3</button></td><td><button id='demo-area-05-btn4' class='db5'>按鈕 4</button></td></tr><tr><td></td><td><button id='demo-area-05-btn5' class='db5'>按鈕 5</button></td><td></td></tr></table>";
 
