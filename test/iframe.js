8ece23cedf1f927c017e623763628354bab7b16a
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-26 19:28:35 +0800

    Update iframe.js

--- test/iframe.js
+++ test/iframe.js
@@ -2,39 +2,36 @@
 
   'use strict';
 
-  function createIframe(input_url_,input_WIDTH_,input_HEIGHT_,input_LEFT_,input_TOP_) {
-    
-    var substr = input_url_.toLowerCase().split('.');
-    var extname_av = ".aac,.au,.aif,.aiff,.aiffc,.alac,.ape,.asf,.avi,.cda,.dat,.divx,.flac,.m2ts,.m4a,.mid,.mov,.mp2,.mp3,.mp4,.mpc,.mpg,.mpeg,.mv2,.ogg,.pdf,.ra,.ram,.raw,.rcp,.rm,.rmvb,.snd,.tak,.tta,.vob,.voc,.wma,.wav,.webm,.wmf,.wmv,.wv,.xmi,";
-    var extname_flash = ".swf,.flv,";
-        
-    if (extname_av.indexOf("."+substr[substr.length-1]+",")!=-1)
-        var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" autostart="true"></embed>'; 
-    else if (extname_flash.indexOf("."+substr[substr.length-1]+",")!=-1)
-        var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" type="application/x-shockwave-flash"></embed>';
-    else
-        var src = '<iframe src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px"></iframe>';
+  function createElement() {
     
     if (document.getElementById('fustyles'))
     {
-          document.getElementById('fustyles').style.left = input_LEFT_ + 'px';
-          document.getElementById('fustyles').style.top = input_TOP_ + 'px';
-          document.getElementById('fustyles').style.display = 'block';
-          document.getElementById('fustyles').innerHTML = src;
+          //
     }
     else
     {
         var div = document.createElement('div');
+        div.id = 'demo-area-01-show';
+        document.body.appendChild(div);
+
+        var list = document.getElementsByClassName("db5");
+         for(var i = list.length - 1; 0 <= i; i--)
+         if(list[i] && list[i].parentElement)
+         list[i].parentElement.removeChild(list[i]);
+
+        var divHtml="<table><tr><td></td><td><button id='demo-area-05-btn1' class='db5'>按鈕 1</button></td><td></td></tr><tr><td><button id='demo-area-05-btn2' class='db5'>按鈕 2</button></td><td><button id='demo-area-05-btn3' class='db5'>按鈕 3</button></td><td><button id='demo-area-05-btn4' class='db5'>按鈕 4</button></td></tr><tr><td></td><td><button id='demo-area-05-btn5' class='db5'>按鈕 5</button></td><td></td></tr></table>";
+
+        var div = document.createElement('div');
         div.id = 'fustyles';
         div.style.position = 'absolute';      
-        div.style.left = input_LEFT_ + 'px';
-        div.style.top = input_TOP_ + 'px';
+        div.style.left = '50px';
+        div.style.top = '100px';
         div.style.zindex='9999';      
-        div.innerHTML = src;
+        div.innerHTML = divHtml;
         document.body.appendChild(div);
     }
   }
 
-  window.createIframe = createIframe;
+  window.createElement = createElement;
 
 }(window, window.document));
