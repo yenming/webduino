10e446580f615c71c74b1ea48b565d4d124ffcc4
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-27 00:19:59 +0800

    Update createbutton.js

--- modifyWebDemo_20180526/createbutton.js
+++ modifyWebDemo_20180526/createbutton.js
@@ -10,7 +10,7 @@
           for(var i = list.length-1;i>=0; i--)
           if(list[i] && list[i].parentElement) list[i].parentElement.removeChild(list[i]);
 
-        var divHtml="<table><tr><td></td><td><button id='demo-area-05-btn1' class='db5'>按鈕 1</button></td><td></td></tr><tr><td><button id='demo-area-05-btn2' class='db5'>按鈕 2</button></td><td><button id='demo-area-05-btn3' class='db5'>按鈕 3</button></td><td><button id='demo-area-05-btn4' class='db5'>按鈕 4</button></td></tr><tr><td></td><td><button id='demo-area-05-btn5' class='db5'>按鈕 5</button></td><td></td></tr></table>";
+        var Html="<table><tr><td></td><td><button id='demo-area-05-btn1' class='db5'>按鈕 1</button></td><td></td></tr><tr><td><button id='demo-area-05-btn2' class='db5'>按鈕 2</button></td><td><button id='demo-area-05-btn3' class='db5'>按鈕 3</button></td><td><button id='demo-area-05-btn4' class='db5'>按鈕 4</button></td></tr><tr><td></td><td><button id='demo-area-05-btn5' class='db5'>按鈕 5</button></td><td></td></tr></table>";
 
         var div = document.createElement('div');
         div.id = 'fustyles';
@@ -18,7 +18,7 @@
         //div.style.left = '50px';
         //div.style.top = '100px';
         //div.style.zindex='9999';      
-        div.innerHTML = divHtml;
+        div.innerHTML = Html;
         document.body.appendChild(div);
     }
     
