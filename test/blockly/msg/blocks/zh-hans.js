a59d28c9bf6c2baa9bb7b006bcc41909337ec7aa
fustyles <34397289+fustyles@users.noreply.github.com>
2018-06-21 00:06:36 +0800

    Update zh-hans.js

--- test/blockly/msg/blocks/zh-hans.js
+++ test/blockly/msg/blocks/zh-hans.js
@@ -2,6 +2,7 @@ Blockly.Msg.MATRIXLED_MATRIX_SHOW = "显示 指示灯";
 Blockly.Msg.MATRIXLED_MATRIXCODE_SHOW = "显示 指示灯";
 Blockly.Msg.MATRIXLED_CLEAR_SHOW = "清除 指示灯";
 Blockly.Msg.MATRIXLED_ON_SHOW = "亮灯";
+Blockly.Msg.MATRIXLED_COLOR_ON_SHOW = "亮灯";
 Blockly.Msg.MATRIXLED_OFF_SHOW = "暗灯";
 Blockly.Msg.MATRIXLED_REVERSE_SHOW = "开关切换";
 Blockly.Msg.MATRIXLED_STATE_SHOW = "指示灯状态";
