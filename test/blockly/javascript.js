d43278763dfcbfdecb0066bb236160123a67cb8b
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-19 13:55:25 +0800

    Add files via upload

--- test/blockly/javascript.js
+++ test/blockly/javascript.js
@@ -12,7 +12,7 @@ Blockly.JavaScript['matrix_led_marquee_time'] = function(block) {
 
 Blockly.JavaScript['matrix_led_marquee'] = function(block) {
   var value_marquee_ = Blockly.JavaScript.valueToCode(block, 'value_marquee_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'MatrixLed_marquee('+ value_marquee_ +');';
+  var code = 'MatrixLed_marquee(matrix,'+ value_marquee_ +');';
   return code;
 };
 
