d6f5cadaa2bbe0932c07707338537012773e6468
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-20 11:52:22 +0800

    Update javascript.js

--- MatrixLed_20180719/blockly/javascript.js
+++ MatrixLed_20180719/blockly/javascript.js
@@ -201,7 +201,7 @@ Blockly.JavaScript['matrix_led_state'] = function(block) {
 
 Blockly.JavaScript['matrix_led_showstate'] = function(block) {
   var value_showstate_ = block.getFieldValue('value_showstate_');
-  var code = 'MatrixLed_showstate(fuMatrixLed,'+value_showstate_+');\n';
+  var code = 'MatrixLed_showstate('+value_showstate_+');\n';
   return code;
 };
 
