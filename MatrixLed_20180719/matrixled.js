3337993e1a90806058a273cb8cc03013ec2208fc
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-20 11:51:33 +0800

    Update matrixled.js

--- MatrixLed_20180719/matrixled.js
+++ MatrixLed_20180719/matrixled.js
@@ -105,14 +105,12 @@
   var MatrixLedmarqueereverse = 1;
   var MatrixLedshowstate = 1;
   var marqueetimeid,marqueetimeid1;
-  var objMatrixLed;
 
   function MatrixLed_color(input_color_) {
     MatrixLedcolor = input_color_;
   }
   
   function MatrixLed_showstate(obj,input_showstate_) {
-    objMatrixLed=obj;
     MatrixLedshowstate = input_showstate_;
   }   
   
