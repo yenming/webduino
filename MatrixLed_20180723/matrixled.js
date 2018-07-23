accd0b6a64ae0e6f63a119e27a9b90f89680ceac
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-23 15:49:59 +0800

    Update matrixled.js

--- MatrixLed_20180723/matrixled.js
+++ MatrixLed_20180723/matrixled.js
@@ -97,14 +97,15 @@
   
   var L = new Array("","","","","","","","","","","","","","","","","","","","","","","","","");
   var MatrixLedcolor = "#ff0000";
-  var MatrixLedbackcolor = "#000000";
+  var MatrixLedbackcolor = "#ffffff";
+  var MatrixLedwidth = 250;
+  var MatrixLedheight = 250;
   var MatrixLedmarqueeinitial = "",MatrixLedmarqueecode = "",MatrixLedmarqueecodeonce = "",MatrixLedmarqueecolorcode = "",MatrixLedmarqueecolorcodeonce = "";  
   var MatrixLedmarqueetime = 500;
   var MatrixLedmarqueereverse = 1;
   var MatrixLedshowstate = 1;
   var marqueetimeid;
   var marqueeactive;
-  var objMatrixLed;
 
   function MatrixLed_color(input_color_) {
     MatrixLedcolor = input_color_;
