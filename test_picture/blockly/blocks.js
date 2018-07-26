994e2fcedbbb41aa617fde10aaf845fd44bc4e46
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-26 20:00:51 +0800

    Update blocks.js

--- test_picture/blockly/blocks.js
+++ test_picture/blockly/blocks.js
@@ -1,10 +1,12 @@
-Blockly.Blocks['iframe_open'] = {
+","picture_width","picture_height","picture_left","picture_top","picture_zindex","picture_display","picture_hide","picture_delete
+
+Blockly.Blocks['picture_create'] = {
   init: function() {
-  this.appendValueInput("openid_")
+  this.appendValueInput("id_")
       .setCheck("Number")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.OPEN_SHOW)
-      .appendField(Blockly.Msg.OPENID_SHOW)    
+      .appendField(Blockly.Msg.CREATE_SHOW)
+      .appendField(Blockly.Msg.ID_SHOW)    
   this.appendValueInput("url_")
       .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
@@ -21,28 +23,68 @@ Blockly.Blocks['iframe_open'] = {
       .setCheck("Number")
       .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(Blockly.Msg.LEFT_SHOW);    
-    this.appendValueInput("top_")
+  this.appendValueInput("top_")
       .setCheck("Number")
       .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(Blockly.Msg.TOP_SHOW);  
+  this.appendValueInput("zindex_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.ZINDEX_SHOW);      
   this.setPreviousStatement(true);
   this.setNextStatement(true);
   this.setTooltip('');
   this.setColour(65);
-  this.setHelpUrl(mainUrl + 'basic/blockly/blockly-information.html' + utmUrl);
   }
 };
-Blockly.Blocks['iframe_close'] = {
+
+Blockly.Blocks['picture_width'] = {
   init: function() {
-    this.appendValueInput("closeid_")
-        .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.CLOSE_SHOW)
-        .appendField(Blockly.Msg.CLOSEID_SHOW)     
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(65);
- this.setTooltip("");
- this.setHelpUrl("");
+  this.appendValueInput("id_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.PICTURE_SHOW)
+      .appendField(Blockly.Msg.ID_SHOW)    
+  this.appendValueInput("width_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.WIDTH_SHOW)
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setTooltip('');
+  this.setColour(65);
+  }
+};
+
+Blockly.Blocks['picture_height'] = {
+  init: function() {
+  this.appendValueInput("id_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.PICTURE_SHOW)
+      .appendField(Blockly.Msg.ID_SHOW)    
+  this.appendValueInput("height_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.HEIGHT_SHOW)
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setTooltip('');
+  this.setColour(65);
+  }
+};
+
+Blockly.Blocks['picture_delete'] = {
+  init: function() {
+  this.appendValueInput("id_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.DELETE_SHOW)
+      .appendField(Blockly.Msg.ID_SHOW)     
+  this.setPreviousStatement(true, null);
+  this.setNextStatement(true, null);
+  this.setColour(65);
+  this.setTooltip("");
+  this.setHelpUrl("");
   }
 };
