16d806712ee1b7b61e003a0a8320c19b5a6dddf5
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-27 01:40:17 +0800

    Update zh-hans.js

--- GamePicture_20180726/blockly/msg/blocks/zh-hans.js
+++ GamePicture_20180726/blockly/msg/blocks/zh-hans.js
@@ -1,5 +1,5 @@
 Blockly.Msg.CREATE_SHOW = "图片 新增";
-Blockly.Msg.ID_SHOW = "序号";
+Blockly.Msg.ID_SHOW = "编号";
 Blockly.Msg.URL_SHOW = "来源网址";
 Blockly.Msg.WIDTH_SHOW = "宽度";
 Blockly.Msg.HEIGHT_SHOW = "高度";
