e59ea1d1fb37d8502128f5bdff34a86214aea7d5
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-26 20:05:44 +0800

    Update blocks.js

--- test_picture/blockly/blocks.js
+++ test_picture/blockly/blocks.js
@@ -5,16 +5,16 @@ Blockly.Blocks['picture_create'] = {
   this.appendValueInput("id_")
       .setCheck("Number")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.CREATE_SHOW)
+      .appendField(Blockly.Msg.CREATE_SHOW);
       .appendField(Blockly.Msg.ID_SHOW)    
   this.appendValueInput("url_")
       .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.URL_SHOW)
+      .appendField(Blockly.Msg.URL_SHOW);
   this.appendValueInput("width_")
       .setCheck("Number")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.WIDTH_SHOW)
+      .appendField(Blockly.Msg.WIDTH_SHOW);
   this.appendValueInput("height_")
       .setCheck("Number")
       .setAlign(Blockly.ALIGN_RIGHT)
@@ -43,10 +43,11 @@ Blockly.Blocks['picture_width'] = {
   this.appendValueInput("id_")
       .setCheck("Number")
       .appendField(Blockly.Msg.PICTURE_SHOW)
-      .appendField(Blockly.Msg.ID_SHOW)    
+      .appendField(Blockly.Msg.ID_SHOW);
   this.appendValueInput("width_")
       .setCheck("Number")
-      .appendField(Blockly.Msg.WIDTH_SHOW)
+      .appendField(Blockly.Msg.WIDTH_SHOW);
+  this.setInputsInline(true);    
   this.setPreviousStatement(true);
   this.setNextStatement(true);
   this.setTooltip('');
@@ -58,11 +59,80 @@ Blockly.Blocks['picture_height'] = {
   init: function() {
   this.appendValueInput("id_")
       .setCheck("Number")
-      .appendField(Blockly.Msg.PICTURE_SHOW)
+      .appendField(Blockly.Msg.PICTURE_SHOW);
       .appendField(Blockly.Msg.ID_SHOW)    
   this.appendValueInput("height_")
       .setCheck("Number")
-      .appendField(Blockly.Msg.HEIGHT_SHOW)
+      .appendField(Blockly.Msg.HEIGHT_SHOW);
+  this.setInputsInline(true);    
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setTooltip('');
+  this.setColour(65);
+  }
+};
+
+Blockly.Blocks['picture_left'] = {
+  init: function() {
+  this.appendValueInput("id_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.PICTURE_SHOW);
+      .appendField(Blockly.Msg.ID_SHOW)    
+  this.appendValueInput("left_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.LEFT_SHOW);
+  this.setInputsInline(true);    
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setTooltip('');
+  this.setColour(65);
+  }
+};
+
+Blockly.Blocks['picture_top'] = {
+  init: function() {
+  this.appendValueInput("id_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.PICTURE_SHOW);
+      .appendField(Blockly.Msg.ID_SHOW)    
+  this.appendValueInput("top_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.TOP_SHOW);
+  this.setInputsInline(true);    
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setTooltip('');
+  this.setColour(65);
+  }
+};
+
+Blockly.Blocks['picture_zindex'] = {
+  init: function() {
+  this.appendValueInput("id_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.PICTURE_SHOW)
+      .appendField(Blockly.Msg.ID_SHOW);
+  this.appendValueInput("zindex_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.ZINDEX_SHOW);
+  this.setInputsInline(true);    
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setTooltip('');
+  this.setColour(65);
+  }
+};
+
+Blockly.Blocks['picture_display'] = {
+  init: function() {
+  this.appendValueInput("id_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.PICTURE_SHOW);
+      .appendField(Blockly.Msg.ID_SHOW)    
+  this.appendValueInput("display_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.DISPLAY_SHOW);
+  this.setInputsInline(true);
   this.setPreviousStatement(true);
   this.setNextStatement(true);
   this.setTooltip('');
@@ -75,7 +145,7 @@ Blockly.Blocks['picture_delete'] = {
   this.appendValueInput("id_")
       .setCheck("Number")
       .appendField(Blockly.Msg.DELETE_SHOW)
-      .appendField(Blockly.Msg.ID_SHOW)     
+      .appendField(Blockly.Msg.ID_SHOW); 
   this.setPreviousStatement(true, null);
   this.setNextStatement(true, null);
   this.setColour(65);
