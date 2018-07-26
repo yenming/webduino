d3ea60a127634e98f2f82d06c7164b7fa4ca235e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-26 22:18:30 +0800

    Update javascript.js

--- test_picture/blockly/javascript.js
+++ test_picture/blockly/javascript.js
@@ -7,7 +7,7 @@ Blockly.JavaScript['picture_create'] = function (block) {
   var value_top_ = Blockly.JavaScript.valueToCode(block, 'top_', Blockly.JavaScript.ORDER_ATOMIC);  
   var value_zindex_ = Blockly.JavaScript.valueToCode(block, 'zindex_', Blockly.JavaScript.ORDER_ATOMIC);    
   var value_display_ = Blockly.JavaScript.valueToCode(block, 'display_', Blockly.JavaScript.ORDER_ATOMIC);  
-  var code = 'picture_create(' + value_id_ + ',' + value_url_ + ','+ value_width_ + ',' + value_height_ + ',' + value_left_ + ',' + value_top_ + ',' + value_zindex_ + ',' + value_display_ + ');';
+  var code = 'picture_create("' + value_id_ + '","' + value_url_ + '",'+ value_width_ + ',' + value_height_ + ',' + value_left_ + ',' + value_top_ + ',' + value_zindex_ + ',' + value_display_ + ');';
   return code;
 };
 
@@ -15,19 +15,19 @@ Blockly.JavaScript['picture_set'] = function (block) {
   var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);  
   var value_property_ = block.getFieldValue('property_');
   var value_value_ = Blockly.JavaScript.valueToCode(block, 'value_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'picture_set(' + value_id_ + ',' + value_property_ + ',' + value_value_ + ');';
+  var code = 'picture_set("' + value_id_ + '","' + value_property_ + '","' + value_value_ + '");';
   return code;
 };
 
 Blockly.JavaScript['picture_get'] = function (block) {
   var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC); 
   var value_property_ = block.getFieldValue('property_');
-  var code = 'picture_get(' + value_id_ + ',' + value_property_ + ')';
+  var code = 'picture_get("' + value_id_ + '","' + value_property_ + '")';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['picture_delete'] = function(block) {
   var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);  
-  var code = 'picture_delete(' + value_id_ + ');';
+  var code = 'picture_delete("' + value_id_ + '");';
   return code;
 };
