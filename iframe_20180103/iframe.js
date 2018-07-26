ef0c52fc4a3cd24c72467d9214409830f95850cb
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-27 01:33:23 +0800

    Add files via upload

--- iframe_20180103/iframe.js
+++ iframe_20180103/iframe.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  function createIframe(input_id_,input_url_,input_WIDTH_,input_HEIGHT_,input_LEFT_,input_TOP_) {
+  function createIframe(input_url_,input_WIDTH_,input_HEIGHT_,input_LEFT_,input_TOP_) {
     
     var substr = input_url_.toLowerCase().split('.');
     var extname_av = ".aac,.au,.aif,.aiff,.aiffc,.alac,.ape,.asf,.avi,.cda,.dat,.divx,.flac,.m2ts,.m4a,.mid,.mov,.mp2,.mp3,.mp4,.mpc,.mpg,.mpeg,.mv2,.ogg,.pdf,.ra,.ram,.raw,.rcp,.rm,.rmvb,.snd,.tak,.tta,.vob,.voc,.wma,.wav,.webm,.wmf,.wmv,.wv,.xmi,";
@@ -15,17 +15,17 @@
     else
         var src = '<iframe src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" allow="geolocation; microphone; camera"></iframe>';
     
-    if (document.getElementById("fustyles_iframe"+input_id_))
+    if (document.getElementById('fustyles'))
     {
-          document.getElementById("fustyles_iframe"+input_id_).style.left = input_LEFT_ + 'px';
-          document.getElementById("fustyles_iframe"+input_id_).style.top = input_TOP_ + 'px';
-          document.getElementById("fustyles_iframe"+input_id_).style.display = 'block';
-          document.getElementById("fustyles_iframe"+input_id_).innerHTML = src;
+          document.getElementById('fustyles').style.left = input_LEFT_ + 'px';
+          document.getElementById('fustyles').style.top = input_TOP_ + 'px';
+          document.getElementById('fustyles').style.display = 'block';
+          document.getElementById('fustyles').innerHTML = src;
     }
     else
     {
         var div = document.createElement('div');
-        div.id = "fustyles_iframe"+input_id_;
+        div.id = 'fustyles';
         div.style.position = 'absolute';      
         div.style.left = input_LEFT_ + 'px';
         div.style.top = input_TOP_ + 'px';
