318ce69d9881ce908efe79cb73cc99c91f7c0f21
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-26 21:54:48 +0800

    Update picture.js

--- test_picture/picture.js
+++ test_picture/picture.js
@@ -4,7 +4,7 @@
 
   function picture_create(input_id,input_url,input_width,input_height,input_left,input_top,input_zindex,input_display) {
 
-    if (document.getElementById("pic_"+input_id_))
+    if (document.getElementById("pic_"+input_id))
     {
       document.getElementById("pic_"+input_id).src = input_url;
       document.getElementById("pic_"+input_id).style.width = input_width + 'px';
@@ -12,7 +12,7 @@
       document.getElementById("pic_"+input_id).style.left = input_left + 'px';
       document.getElementById("pic_"+input_id).style.top = input_top + 'px';
       document.getElementById("pic_"+input_id).style.zindex = input_zindex;
-      if (input_display==flase)
+      if (input_display==false)
         document.getElementById("pic_"+input_id).style.display = "none";
       else
         document.getElementById("pic_"+input_id).style.display = "block";
@@ -20,7 +20,7 @@
     else
     {
       var pic = document.createElement('img');
-      pic.style.position="absolute";
+      pic.style.position = "absolute";
       pic.id = "pic_"+input_id;
       pic.src = input_url;
       pic.style.width = input_width + 'px';
@@ -28,7 +28,7 @@
       pic.style.left = input_left + 'px';
       pic.style.top = input_top + 'px';
       pic.style.zindex = input_zindex;
-      if (input_display==flase)
+      if (input_display==false)
         pic.style.display = "none";
       else
         pic.style.display = "block";
@@ -37,50 +37,49 @@
     }
   }
   
-  function picture_set(input_id,input_property) {
+  function picture_set(input_id,input_property,input_value) {
 
-    if (document.getElementById("pic_"+input_id_))
+    if (document.getElementById("pic_"+input_id))
     {
       if (input_property=='url')
-        return document.getElementById("pic_"+input_id).src = input_url;
-      esle if (input_property=='width')
-        return document.getElementById("pic_"+input_id).style.width = input_width + 'px';
-      esle if (input_property=='height')
-        return document.getElementById("pic_"+input_id).style.height = input_height + 'px';
-      esle if (input_property=='left')
-        return document.getElementById("pic_"+input_id).style.left = input_left + 'px';
-      esle if (input_property=='top')
-        return document.getElementById("pic_"+input_id).style.top = input_top + 'px';
-      esle if (input_property=='zindex')
-        return document.getElementById("pic_"+input_id).style.zindex = input_zindex;
-      esle if (input_property=='display')
+        document.getElementById("pic_"+input_id).src = input_value;
+      else if (input_property=='width')
+        document.getElementById("pic_"+input_id).style.width = input_value + 'px';
+      else if (input_property=='height')
+        document.getElementById("pic_"+input_id).style.height = input_value + 'px';
+      else if (input_property=='left')
+        document.getElementById("pic_"+input_id).style.left = input_value + 'px';
+      else if (input_property=='top')
+        document.getElementById("pic_"+input_id).style.top = input_value + 'px';
+      else if (input_property=='zindex')
+        document.getElementById("pic_"+input_id).style.zindex = input_value;
+      else if (input_property=='display')
       {
-        if (input_display==flase)
+        if (input_value==false)
           document.getElementById("pic_"+input_id).style.display = "none";
         else
           document.getElementById("pic_"+input_id).style.display = "block";
       }
     }
-
   }    
   
   function picture_get(input_id,input_property) {
 
-    if (document.getElementById("pic_"+input_id_))
+    if (document.getElementById("pic_"+input_id))
     {
       if (input_property=='url')
         return document.getElementById("pic_"+input_id).src;
-      esle if (input_property=='width')
+      else if (input_property=='width')
         return document.getElementById("pic_"+input_id).style.width;
-      esle if (input_property=='height')
+      else if (input_property=='height')
         return document.getElementById("pic_"+input_id).style.height;
-      esle if (input_property=='left')
+      else if (input_property=='left')
         return document.getElementById("pic_"+input_id).style.left;
-      esle if (input_property=='top')
+      else if (input_property=='top')
         return document.getElementById("pic_"+input_id).style.top;
-      esle if (input_property=='zindex')
+      else if (input_property=='zindex')
         return document.getElementById("pic_"+input_id).style.zindex;
-      esle if (input_property=='display')
+      else if (input_property=='display')
         return document.getElementById("pic_"+input_id).style.display;
     }
     else
