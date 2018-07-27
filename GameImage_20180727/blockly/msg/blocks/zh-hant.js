19353f6ebd3dbe05591f79949f75c0408aff997c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-27 17:48:00 +0800

    Update zh-hant.js

--- GameImage_20180727/blockly/msg/blocks/zh-hant.js
+++ GameImage_20180727/blockly/msg/blocks/zh-hant.js
@@ -1,5 +1,5 @@
 Blockly.Msg.CREATE_SHOW = "圖片新增";
-Blockly.Msg.ID_SHOW = "編號";
+Blockly.Msg.ID_SHOW = "識別碼";
 Blockly.Msg.URL_SHOW = "來源網址";
 Blockly.Msg.WIDTH_SHOW = "寬度";
 Blockly.Msg.HEIGHT_SHOW = "高度";
