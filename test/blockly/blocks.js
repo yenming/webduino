21ede201c6390216681e71a1a258006ee736f617
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-27 18:34:20 +0800

    Update blocks.js

--- test/blockly/blocks.js
+++ test/blockly/blocks.js
@@ -45,11 +45,12 @@ Blockly.Blocks['image_set'] = {
       .setAlign(Blockly.ALIGN_RIGHT)
       .setCheck(null)    
       .appendField(Blockly.Msg.SET_SHOW)
+      .appendField(new Blockly.FieldDropdown([["url","url"], ["width","width"], ["height","height"], ["left","left"], ["top","top"], ["zindex","zindex"], ["display","display"]]), "property_")    
       .appendField(Blockly.Msg.ID_SHOW);
   this.appendValueInput("value_")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .setCheck(null)
-      .appendField(new Blockly.FieldDropdown([["url","url"], ["width","width"], ["height","height"], ["left","left"], ["top","top"], ["zindex","zindex"], ["display","display"]]), "property_");
+      .setCheck(null);
+  this.setInputsInline(true);
   this.setPreviousStatement(true);
   this.setNextStatement(true);
   this.setColour(65);
