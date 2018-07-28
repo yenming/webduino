8170eb76f176e24d597996670791fbed1b6b18f3
fustyles <34397289+fustyles@users.noreply.github.com>
2018-07-28 14:21:52 +0800

    Update iframe.js

--- test/iframe.js
+++ test/iframe.js
@@ -14,7 +14,7 @@
     else if (extname_flash.indexOf("."+substr[substr.length-1]+",")!=-1)
         var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" type="application/x-shockwave-flash"></embed>';
     else if (extname_text.indexOf("<text>")==0)
-        var src = input_url_.substr(6,(input_url_.length()-6));
+        var src = input_url_.substr(6,(input_url_.length-6));
     else
         var src = '<iframe src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" frameborder="0" allow="geolocation; microphone; camera"></iframe>';
     
