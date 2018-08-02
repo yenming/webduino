50d12bcb2e632353681d69086b2389510415dc95
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-02 18:00:49 +0800

    Update gameimage.js

--- GameImage_20180802/gameimage.js
+++ GameImage_20180802/gameimage.js
@@ -181,7 +181,7 @@
   }
   
   function image_onclick_get(input_id) {
-    if ((input_id!="")&&(onclickid=="gameimg_"+input_id))
+    if ((input_id!="")&&(onclickid==("gameimg_"+input_id)))
     {
       onclickid="";
       return 1;
