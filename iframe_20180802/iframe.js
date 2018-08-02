cd1a2359c1819b2398bce5dcb58059f35b04d340
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-02 14:20:32 +0800

    Update iframe.js

--- iframe_20180802/iframe.js
+++ iframe_20180802/iframe.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  function createIframe(input_id_,input_url_,input_WIDTH_,input_HEIGHT_,input_LEFT_,input_TOP_) {
+  function createIframe(input_id_,input_url_,input_width_,input_height_,input_left_,input_top_,input_zindex,input_display) {
     
     var substr = input_url_.toLowerCase().split('.');
     var extname_av = ".aac,.au,.aif,.aiff,.aiffc,.alac,.ape,.asf,.avi,.cda,.dat,.divx,.flac,.m2ts,.m4a,.mid,.mov,.mp2,.mp3,.mp4,.mpc,.mpg,.mpeg,.mv2,.ogg,.pdf,.ra,.ram,.raw,.rcp,.rm,.rmvb,.snd,.tak,.tta,.vob,.voc,.wma,.wav,.webm,.wmf,.wmv,.wv,.xmi,";
@@ -11,17 +11,21 @@
     if (input_url_.toLowerCase().indexOf("http")!=0)
         var src = input_url_;
     else if (extname_av.indexOf("."+substr[substr.length-1]+",")!=-1)
-        var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" autostart="true"></embed>'; 
+        var src = '<embed src="' + input_url_ + '" style="width:' + input_width_ + 'px;height:' + input_height_ + 'px" autostart="true"></embed>'; 
     else if (extname_flash.indexOf("."+substr[substr.length-1]+",")!=-1)
-        var src = '<embed src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" type="application/x-shockwave-flash"></embed>';
+        var src = '<embed src="' + input_url_ + '" style="width:' + input_width_ + 'px;height:' + input_height_ + 'px" type="application/x-shockwave-flash"></embed>';
     else 
-        var src = '<iframe src="' + input_url_ + '" style="width:' + input_WIDTH_ + 'px;height:' + input_HEIGHT_ + 'px" frameborder="0" allow="geolocation; microphone; camera"></iframe>';
+        var src = '<iframe src="' + input_url_ + '" style="width:' + input_width_ + 'px;height:' + input_height_ + 'px" frameborder="0" allow="geolocation; microphone; camera"></iframe>';
     
     if (document.getElementById("fustyles_iframe"+input_id_))
     {
-          document.getElementById("fustyles_iframe"+input_id_).style.left = input_LEFT_ + 'px';
-          document.getElementById("fustyles_iframe"+input_id_).style.top = input_TOP_ + 'px';
-          document.getElementById("fustyles_iframe"+input_id_).style.display = 'block';
+          document.getElementById("fustyles_iframe"+input_id_).style.left = input_left_ + 'px';
+          document.getElementById("fustyles_iframe"+input_id_).style.top = input_top_ + 'px';
+          document.getElementById("fustyles_iframe"+input_id_).style.zIndex = input_zindex;
+          if (input_display==true)
+            document.getElementById("fustyles_iframe"+input_id_).style.display = 'block';
+          else
+            document.getElementById("fustyles_iframe"+input_id_).style.display = 'none';
           document.getElementById("fustyles_iframe"+input_id_).innerHTML = src;
     }
     else
@@ -29,9 +33,9 @@
         var div = document.createElement('div');
         div.id = "fustyles_iframe"+input_id_;
         div.style.position = 'absolute';      
-        div.style.left = input_LEFT_ + 'px';
-        div.style.top = input_TOP_ + 'px';
-        div.style.zIndex='9999';      
+        div.style.left = input_left_ + 'px';
+        div.style.top = input_top_ + 'px';
+        div.style.zIndex = input_zindex;      
         div.innerHTML = src;
         document.body.appendChild(div);
     }
