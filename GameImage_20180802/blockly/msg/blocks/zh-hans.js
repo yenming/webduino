dffb8c400311c6bed0b14fcbe03962012e6399e0
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-02 15:51:00 +0800

    Update zh-hans.js

--- GameImage_20180802/blockly/msg/blocks/zh-hans.js
+++ GameImage_20180802/blockly/msg/blocks/zh-hans.js
@@ -15,3 +15,4 @@ Blockly.Msg.AND_SHOW = " 与 ";
 Blockly.Msg.BOUNDARY_SHOW = "设定视窗大小";
 Blockly.Msg.BOUNDARY_COLLISION_SHOW = "取得图片 边界状态";
 Blockly.Msg.SYS_GET_SHOW = "取得视窗";
+Blockly.Msg.ONCLICK_SHOW = "取得图片 点选状态";
