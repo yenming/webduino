4cd82eb9b12389d75a630eb368100723e9f17780
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-06 14:23:02 +0800

    Update blocks.js

--- GameImage_20180802/blockly/blocks.js
+++ GameImage_20180802/blockly/blocks.js
@@ -134,7 +134,7 @@ Blockly.Blocks['image_sys_get'] = {
   init: function() {
   this.appendDummyInput()
       .appendField(Blockly.Msg.SYS_GET_SHOW)
-      .appendField(new Blockly.FieldDropdown([["screen_width","screen_width"], ["screen_height","screen_height"]]), "property_");
+      .appendField(new Blockly.FieldDropdown([["width","screen_width"], ["height","screen_height"]]), "property_");
   this.setInputsInline(true);
   this.setOutput(true, null); 
   this.setColour(60);
