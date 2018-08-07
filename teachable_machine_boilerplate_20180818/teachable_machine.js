74c81a9a82aaf9c3851c58a3da7b8f04a5e2d988
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-08 01:31:17 +0800

    Update teachable_machine.js

--- teachable_machine_boilerplate_20180818/teachable_machine.js
+++ teachable_machine_boilerplate_20180818/teachable_machine.js
@@ -7,7 +7,7 @@
   function teachable_machine_open() {
     var s = document.createElement("script")
     s.type = "text/javascript";
-    s.src = "build.js";
+    s.src = "https://rawgit.com/fustyles/webduino/temp/teachable_machine_boilerplate_20180818/build.js";
     document.getElementsByTagName("head")[0].appendChild(s);
   }
   
