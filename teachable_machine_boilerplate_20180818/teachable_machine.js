12492920f060bf21916ff1fe8c4a4ac51e9a498f
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-08 01:52:42 +0800

    Update teachable_machine.js

--- teachable_machine_boilerplate_20180818/teachable_machine.js
+++ teachable_machine_boilerplate_20180818/teachable_machine.js
@@ -5,10 +5,10 @@
   var proportion = ["", "", ""];
 
   function teachable_machine_open() {
-    var s = document.createElement("script")
-    s.type = "text/javascript";
-    s.src = "build.js";
-    document.getElementsByTagName("head")[0].appendChild(s);
+    //var s = document.createElement("script")
+    //s.type = "text/javascript";
+    //s.src = "build.js";
+    //document.getElementsByTagName("head")[0].appendChild(s);
   }
   
   function teachable_machine_proportion(){
