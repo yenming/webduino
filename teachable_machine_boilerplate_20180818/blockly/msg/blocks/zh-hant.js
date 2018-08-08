fa4d3132cc59a8aec190cfd1777d03bf1d46442b
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-08 11:38:49 +0800

    Update zh-hant.js

--- teachable_machine_boilerplate_20180818/blockly/msg/blocks/zh-hant.js
+++ teachable_machine_boilerplate_20180818/blockly/msg/blocks/zh-hant.js
@@ -1,2 +1,2 @@
-Blockly.Msg.OPEN_SHOW = "機械學習 開啟";
-Blockly.Msg.PROPORTION_SHOW = "機械學習 判斷結果";
+Blockly.Msg.TEACHABLE_MACHINE_OPEN_SHOW = "機械學習 開啟";
+Blockly.Msg.TEACHABLE_MACHINE_PROPORTION_SHOW = "機械學習 判斷結果";
