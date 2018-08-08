22c0f27bdcd870023d0c887e7971fb4b6ebe9399
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-08 11:32:54 +0800

    Update blocks.js

--- teachable_machine_boilerplate_20180818/blockly/blocks.js
+++ teachable_machine_boilerplate_20180818/blockly/blocks.js
@@ -1,7 +1,7 @@
 Blockly.Blocks['teachable_machine_open'] = {
   init: function() {
   this.appendDummyInput()  
-      .appendField(Blockly.Msg.OPEN_SHOW);      
+      .appendField(Blockly.Msg.TEACHABLE_MACHINE_OPEN_SHOW);      
   this.setPreviousStatement(true);
   this.setNextStatement(true);
   this.setColour(65);
@@ -11,7 +11,7 @@ Blockly.Blocks['teachable_machine_open'] = {
 Blockly.Blocks['teachable_machine_proportion'] = {
   init: function() {
   this.appendDummyInput()  
-      .appendField(Blockly.Msg.PROPORTION_SHOW);      
+      .appendField(Blockly.Msg.TEACHABLE_MACHINE_PROPORTION_SHOW);      
   this.setInputsInline(true);
   this.setOutput(true, null); 
   this.setColour(65);
