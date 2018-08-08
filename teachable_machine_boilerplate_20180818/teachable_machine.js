3ea088793a0d7214471efabdbe3666458afe0a17
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-08 09:29:02 +0800

    Update teachable_machine.js

--- teachable_machine_boilerplate_20180818/teachable_machine.js
+++ teachable_machine_boilerplate_20180818/teachable_machine.js
@@ -3,6 +3,27 @@
   'use strict';
 
   function teachable_machine_open() {
+    if (document.getElementById("train"))
+    {
+      document.getElementById("train")[0].innerHTML = "";
+      document.getElementById("train")[1].innerHTML = "";
+      document.getElementById("train")[2].innerHTML = "";
+    }
+    else
+    {
+      var div = document.createElement('div');
+      div.id = "train";
+      div.style.position = 'absolute';      
+      div.style.left = '-500px';
+      div.style.top = '500px';
+      div.style.zIndex = -1;   
+      div.style.display = 'none';
+      div.innerHTML = "";
+      document.body.appendChild(div);
+      document.body.appendChild(div);
+      document.body.appendChild(div);
+    }
+    
     var s = document.createElement("script")
     s.src = "https://rawgit.com/fustyles/webduino/temp/teachable_machine_boilerplate_20180818/build.js";
     document.getElementsByTagName("head")[0].appendChild(s);
