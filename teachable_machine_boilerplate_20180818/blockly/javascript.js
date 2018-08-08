804a69148145c6a43f83a5c7f92cf85887178f35
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-08 10:19:01 +0800

    Update javascript.js

--- teachable_machine_boilerplate_20180818/blockly/javascript.js
+++ teachable_machine_boilerplate_20180818/blockly/javascript.js
@@ -1,9 +1,9 @@
 Blockly.JavaScript['teachable_machine_open'] = function (block) {
-  var code = 'teachable_machine_open();';
+  var code = 'teachable_machine_open();\n';
   return code;
 };
 
 Blockly.JavaScript['teachable_machine_proportion'] = function(block) { 
-  var code = 'teachable_machine_proportion();';
-  return code;
+  var code = 'teachable_machine_proportion()';
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
