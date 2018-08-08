08d63d2eb650aa53adb412354dea6787079b9d53
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-08 09:50:09 +0800

    Update teachable_machine.js

--- teachable_machine_boilerplate_20180818/teachable_machine.js
+++ teachable_machine_boilerplate_20180818/teachable_machine.js
@@ -3,31 +3,25 @@
   'use strict';
 
   function teachable_machine_open() {
-    if (document.getElementById("train0"))
+    if (document.getElementById("train"))
     {
-      document.getElementById("train0").innerHTML = "";
-      document.getElementById("train1").innerHTML = "";
-      document.getElementById("train2").innerHTML = "";
+      document.getElementById("train").childNodes[0].innerHTML = "";
+      document.getElementById("train").childNodes[1].innerHTML = "";
+      document.getElementById("train").childNodes[2].innerHTML = "";
     }
     else
     {
       var div = document.createElement('div');
+      div.id = "train";
       div.style.position = 'absolute';    
       div.style.top = '0px';
+      div.style.left = '200px';
       div.style.zIndex = -1;   
       div.style.display = 'block';
+      div.innerHTML = "train";
       
-      div.id = "train0";
-      div.innerHTML = "train0";
-      div.style.left = '200px';
       document.body.appendChild(div);
-      div.id = "train1";
-      div.innerHTML = "train1";
-      div.style.left = '300px';
       document.body.appendChild(div);
-      div.id = "train2";
-      div.innerHTML = "train2";
-      div.style.left = '400px';
       document.body.appendChild(div);
     }
     
