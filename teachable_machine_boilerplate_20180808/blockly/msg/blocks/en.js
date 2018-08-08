21eaa3b230dadbb0ea1d6f76a299dda68fc7b78c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-08 14:11:40 +0800

    Add files via upload

---
+++ teachable_machine_boilerplate_20180808/blockly/msg/blocks/en.js
@@ -0,0 +1,2 @@
+Blockly.Msg.TEACHABLE_MACHINE_OPEN_SHOW = "Teachable Machine Open";
+Blockly.Msg.TEACHABLE_MACHINE_PROPORTION_SHOW = "Teachable Machine Answer";
---
+++ teachable_machine_boilerplate_20180808/blockly/msg/blocks/zh-hant.js
@@ -0,0 +1,2 @@
+Blockly.Msg.TEACHABLE_MACHINE_OPEN_SHOW = "機械學習 開啟";
+Blockly.Msg.TEACHABLE_MACHINE_PROPORTION_SHOW = "機械學習 判斷結果";
