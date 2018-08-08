b57af1fb667cfb1421655bfefe96fb8c06e7a402
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-08 22:07:40 +0800

    Update en.js

--- teachable_machine_boilerplate_20180808/blockly/msg/blocks/en.js
+++ teachable_machine_boilerplate_20180808/blockly/msg/blocks/en.js
@@ -1,2 +1,2 @@
-Blockly.Msg.TEACHABLE_MACHINE_OPEN_SHOW = "Teachable Machine Open";
-Blockly.Msg.TEACHABLE_MACHINE_PROPORTION_SHOW = "Teachable Machine Answer";
+Blockly.Msg.TEACHABLE_MACHINE_OPEN_SHOW = "Deep Learning Initialize";
+Blockly.Msg.TEACHABLE_MACHINE_PROPORTION_SHOW = "Deep Learning Max Probability";
