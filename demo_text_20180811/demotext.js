70bec2b228f78523ae640914fe9928c4bf5c13d9
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-11 17:26:58 +0800

    Update demotext.js

--- demo_text_20180811/demotext.js
+++ demo_text_20180811/demotext.js
@@ -13,7 +13,53 @@
         document.body.appendChild(div);
     }
   }
+  
+    var demoText = function (id) {
+    if (!id) {
+      throw new Error('Missing parameter.');
+    }
+    if (typeof id !== 'string') {
+      throw new Error('Invalid type of parameter.');
+    }
+
+    this.blockId = '#' + id;
+    this.paragraph = null;
+
+    if ($(this.blockId).length < 1) {
+      this.demoWrapper = $('<div/>').addClass('demo').attr('id', id);
+      $('#demo-container').append(this.demoWrapper);
+      this.paragraph = $('<p/>').appendTo(this.blockId);
+    }
+  };
+
+  demoText.prototype.setText = function (text) {
+    this.paragraph.text(text);
+    return this;
+  };
+
+  demoText.prototype.appendText = function (text) {
+    this.paragraph.append(text);
+    return this;
+  };
+
+  demoText.prototype.setColor = function (hex) {
+    this.paragraph.css('color', hex);
+    return this;
+  };
+
+  demoText.prototype.setSize = function (size) {
+    size = !size ? '' : size += 'px';
+    this.paragraph.css('font-size', size);
+    return this;
+  };
+
+  demoText.prototype.setAlignment = function (alignment) {
+    this.paragraph.css('text-align', alignment);
+    return this;
+  };
+
+  window.demoText = demoText;
 
   window.createDemoText = createDemoText;
 
-}(window, window.document));
+}(window, window.document, window.jQuery));
