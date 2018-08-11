f3808ee5d22a92ecf4dea4f7b463c7914d207c44
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-11 17:35:05 +0800

    Update demotext.js

--- demo_text_20180811/demotext.js
+++ demo_text_20180811/demotext.js
@@ -3,7 +3,7 @@
   'use strict';
 
   function createDemoText() {
-    if (document.getElementById("demo-area-01-show"))
+    if (!document.getElementById("demo-area-01-show"))
     {
         var div = document.createElement('div');
         div.id = "demo-area-01-show";
