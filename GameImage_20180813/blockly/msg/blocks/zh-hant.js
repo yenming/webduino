fa742ebffa49dbc8e8946c967e8983f9d3aded96
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-14 11:51:52 +0800

    Update zh-hant.js

--- GameImage_20180813/blockly/msg/blocks/zh-hant.js
+++ GameImage_20180813/blockly/msg/blocks/zh-hant.js
@@ -17,5 +17,5 @@ Blockly.Msg.BOUNDARY_COLLISION_SHOW = "取得圖片 邊界狀態";
 Blockly.Msg.SYS_GET_SHOW = "取得視窗 ";
 Blockly.Msg.ONCLICK_SHOW = "取得圖片 點選狀態";
 Blockly.Msg.MOUSE_COORDINATE_GET_SHOW = "取得滑鼠 座標";
-Blockly.Msg.DOCUMENT_TIMER_SHOW = "定時器 間隔時間(ms)";
-Blockly.Msg.DOCUMENT_TIMER_STOP_SHOW = "停止定時器";
+Blockly.Msg.DOCUMENT_TIMER_SHOW = "計時器 間隔時間(ms)";
+Blockly.Msg.DOCUMENT_TIMER_STOP_SHOW = "停止計時器";
