f861b60aeec37074aec18d68e355b63383ee8261
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-18 19:00:47 +0800

    Update javascript.js

--- speak_setting_sample_fustyles/blockly/javascript.js
+++ speak_setting_sample_fustyles/blockly/javascript.js
@@ -3,7 +3,7 @@ Blockly.JavaScript['speak_setting_sample_fustyles'] = function (block) {
   var dropdown_volume_ = block.getFieldValue('volume_');
   var dropdown_pitch_ = block.getFieldValue('pitch_');
   var dropdown_rate_ = block.getFieldValue('rate_');
-  var code = '"' + value_lang_ + '",' + dropdown_volume_ + ',' + dropdown_pitch_ + ',' + dropdown_rate_;
+  var code = '"' + dropdown_lang_ + '",' + dropdown_volume_ + ',' + dropdown_pitch_ + ',' + dropdown_rate_;
   return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
