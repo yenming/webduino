d3a289a6677769054ed71af441f6906f6c3d314a
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-21 20:30:17 +0800

    Update blocks.js

--- test_GameElements/blockly/blocks.js
+++ test_GameElements/blockly/blocks.js
@@ -1,9 +1,46 @@
+Blockly.Blocks['canvas_create'] = {
+  init: function() {
+  this.appendValueInput("id_")
+      .setCheck(null)
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.CANVAS_CREATE_SHOW)
+      .appendField(Blockly.Msg.ID_SHOW); 
+  this.appendValueInput("width_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.WIDTH_SHOW);
+  this.appendValueInput("height_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.HEIGHT_SHOW);
+  this.appendValueInput("left_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.LEFT_SHOW);    
+  this.appendValueInput("top_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.TOP_SHOW);  
+  this.appendValueInput("zindex_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.ZINDEX_SHOW);      
+  this.appendValueInput("display_")
+      .setCheck("Boolean")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.DISPLAY_SHOW);        
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
+
 Blockly.Blocks['image_create'] = {
   init: function() {
   this.appendValueInput("id_")
       .setCheck(null)
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.CREATE_SHOW)
+      .appendField(Blockly.Msg.IMAGE_CREATE_SHOW)
       .appendField(Blockly.Msg.ID_SHOW); 
   this.appendValueInput("url_")
       .setCheck("String")
