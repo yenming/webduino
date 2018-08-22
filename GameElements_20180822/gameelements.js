26fe34592453ffe494792c534ab04e5e1b82fafd
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-22 21:31:06 +0800

    Update gameelements.js

--- GameElements_20180822/gameelements.js
+++ GameElements_20180822/gameelements.js
@@ -49,14 +49,14 @@
     }
   } 
   
-  function canvas_rect(input_id,input_linewidth,input_x0,input_y0,input_x1,input_y1,input_fill,input_color) {
+  function canvas_rect(input_id,input_linewidth,input_x0,input_y0,input_width,input_height,input_fill,input_color) {
     if (document.getElementById("gamecanvas_"+input_id)) {
       var context = document.getElementById("gamecanvas_"+input_id).getContext("2d");
       context.strokeStyle = input_color;
       context.fillStyle = input_color;
       context.lineWidth = input_linewidth;
       context.beginPath();
-      context.rect(input_x0,input_y0,input_x1,input_y1);
+      context.rect(input_x0,input_y0,input_width,input_height);
       if (input_fill==0)
         context.stroke();
       else
