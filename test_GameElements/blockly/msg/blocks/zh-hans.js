d383f26da63b3f0dc37eade87ffb7c805d8d604e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-25 15:19:31 +0800

    Update zh-hans.js

--- test_GameElements/blockly/msg/blocks/zh-hans.js
+++ test_GameElements/blockly/msg/blocks/zh-hans.js
@@ -1,3 +1,10 @@
+Blockly.Msg.TABLE_CREATE_SHOW = "表格 新增";
+Blockly.Msg.TABLE_TRCOUNT_SHOW = "列数";
+Blockly.Msg.TABLE_TDCOUNT_SHOW = "行数";
+Blockly.Msg.TABLE_BORDERSTYLE_SHOW = "边框样式";
+Blockly.Msg.TABLE_BORDERWIDTH_SHOW = "边框宽度";
+Blockly.Msg.TABLE_BORDERCOLOR_SHOW = "边框颜色";
+Blockly.Msg.TABLE_BGCOLOR_SHOW = "背景颜色";
 Blockly.Msg.MUSIC_CREATE_SHOW ="背景音乐 播放";
 Blockly.Msg.MUSIC_LENGTH_SHOW ="长度(ms)";
 Blockly.Msg.MUSIC_LOOP_SHOW ="循环";
