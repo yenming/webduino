908dab7fd60ba190e0b53f400e0e92beff5f384c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-25 21:48:02 +0800

    Update gameelements.js

--- test_GameElements/gameelements.js
+++ test_GameElements/gameelements.js
@@ -40,18 +40,13 @@
       document.body.appendChild(tbl);
     }
   }
-  
-  function table_delete(input_id) {
-    if (document.getElementById("gametable_"+input_id))
-      document.getElementById("gametable_"+input_id).parentNode.removeChild(document.getElementById("gametable_"+input_id));
-  }
-  
+
   function table_td_insert_img(input_id,input_x,input_y,input_img_id,input_url,input_width,input_height){
     if (document.getElementById("gametable_td_"+input_id+"_"+input_y+"_"+input_x))
       document.getElementById("gametable_td_"+input_id+"_"+input_y+"_"+input_x).innerHTML = "<img id='gameimg_"+input_img_id+"' src='"+input_url+"' width='"+input_width+"' height='"+input_height+"' onclick='image_onclickid_set(this);'>";
   }
   
-  function table_td_img_get(input_img_id,input_position){
+  function table_td_img_get(input_img_id,input_property){
     if (document.getElementById("gameimg_"+input_img_id))
       return "OK";
     else
@@ -64,11 +59,23 @@
     }
   }  
   
+  function table_td_get(input_id,input_x,input_y,input_property){
+    if (document.getElementById("gametable_td_"+input_id+"_"+input_y+'_'+input_x))
+      return "OK";
+    else
+      return "";
+  }
+  
   function table_td_clear(input_id,input_x,input_y){
     if (document.getElementById("gametable_td_"+input_id+"_"+input_y+"_"+input_x)) {
       document.getElementById("gametable_td_"+input_id+"_"+input_y+"_"+input_x).innerHTML = "";
     }    
   }  
+  
+  function table_delete(input_id) {
+    if (document.getElementById("gametable_"+input_id))
+      document.getElementById("gametable_"+input_id).parentNode.removeChild(document.getElementById("gametable_"+input_id));
+  }
   
   function music_create(input_url) {
     var substr = input_url.toLowerCase().split('.');
@@ -433,6 +440,7 @@
   window.table_td_insert_img = table_td_insert_img;
   window.table_td_img_get = table_td_img_get;
   window.table_td_insert_text = table_td_insert_text;
+  window.table_td_get = table_td_get;
   window.table_td_clear = table_td_clear;
   
 }(window, window.document));
