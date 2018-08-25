aef3d113a733444a73513cbf69f228e874f554cb
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-25 15:12:00 +0800

    Update blocks.js

--- test_GameElements/blockly/blocks.js
+++ test_GameElements/blockly/blocks.js
@@ -28,7 +28,7 @@ Blockly.Blocks['table_create'] = {
       .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(Blockly.Msg.TABLE_TDCOUNT_SHOW);    
 this.appendDummyInput()  
-      .appendField(Blockly.Msg.CANVAS_TEXTALIGN_SHOW)
+      .appendField(Blockly.Msg.TABLE_BORDER_SHOW)
       .appendField(new Blockly.FieldDropdown([["right","right"]]), "border_");   
   this.appendValueInput("borderwidth_")
       .setCheck(null)
