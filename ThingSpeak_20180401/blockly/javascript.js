5cd48ca5d44e427ca9baaee367ede42e2b8137f3
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-27 17:40:52 +0800

    Add files via upload

--- ThingSpeak_20180401/blockly/javascript.js
+++ ThingSpeak_20180401/blockly/javascript.js
@@ -8,6 +8,6 @@ Blockly.JavaScript['thingspeak_update'] = function (block) {
   var field6 = Blockly.JavaScript.valueToCode(block, 'field6', Blockly.JavaScript.ORDER_ATOMIC);
   var field7 = Blockly.JavaScript.valueToCode(block, 'field7', Blockly.JavaScript.ORDER_ATOMIC);
   var field8 = Blockly.JavaScript.valueToCode(block, 'field8', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'createIframe('+key+','+field1+','+field2+','+field3+','+field4+','+field5+','+field6+','+field7+','+field8+');';
+  var code = 'createIframe('+key+','+field1+','+field2+','+field3+','+field4+','+field5+','+field6+','+field7+','+field8+');\n';
   return code; 
 };
--- modifyWebDemo_20180526/blockly/javascript.js
+++ modifyWebDemo_20180526/blockly/javascript.js
@@ -1,4 +1,4 @@
 Blockly.JavaScript['create_button'] = function(block) {
-  var code = 'createButton();';
+  var code = 'createButton();\n';
   return code;
 };
