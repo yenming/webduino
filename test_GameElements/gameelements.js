44fe12bb389c56a1a6783bab2ebf1fa04167f33d
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-27 19:22:06 +0800

    Update gameelements.js

--- test_GameElements/gameelements.js
+++ test_GameElements/gameelements.js
@@ -453,7 +453,7 @@
     else if (input_property=="onclick"){
       if (onclickid.indexOf("gameimg_")==0)   
         return onclickid.replace(/gameimg_/ig,"");
-      else if (onclickid.indexOf("gametable_td_"+input_id)==0){     
+      else if (onclickid.indexOf("gametable_td_")==0){     
           if (document.getElementById(onclickid).hasChildNodes())
             return document.getElementById(onclickid).firstChild.id.replace(/gameimg_/ig,"");
           else
