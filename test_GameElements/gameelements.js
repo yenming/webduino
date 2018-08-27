a32940ee06a2a9e510f71082f89d5cdc20abe8fb
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-27 18:31:42 +0800

    Update gameelements.js

--- test_GameElements/gameelements.js
+++ test_GameElements/gameelements.js
@@ -86,10 +86,12 @@
   }
   
   function table_get(input_id,input_property){
+    /*
     if (input_property=="onclickImage"){
       if (onclickid.indexOf("gameimg_")==0)   
         return onclickid.replace(/gameimg_/ig,"");
     }
+    */
     if (document.getElementById("gametable_"+input_id)) {
       if (input_property=="rows")
         return document.getElementById("gametable_"+input_id).rows.length;
