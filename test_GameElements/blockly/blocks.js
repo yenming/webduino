16018b929a044455e2a83aae5e595f4295f36d69
fustyles <34397289+fustyles@users.noreply.github.com>
2018-09-10 19:21:07 +0800

    Update blocks.js

--- test_GameElements/blockly/blocks.js
+++ test_GameElements/blockly/blocks.js
@@ -129,9 +129,9 @@ Blockly.Blocks['table_td_set'] = {
 Blockly.Blocks['table_border_set'] = {
   init: function() {
   this.appendDummyInput()
-      .appendField(Blockly.Msg.TABLE_BORDER_SET_SHOW)
       .appendField(new Blockly.FieldVariable('fuGameTable'), 'fuGameElements_');    
   this.appendDummyInput()  
+      .appendField(Blockly.Msg.TABLE_BORDER_SET_SHOW)
       .appendField(Blockly.Msg.TABLE_BORDERSTYLE_SHOW)
       .appendField(new Blockly.FieldDropdown([["solid","solid"], ["dashed","dashed"], ["double","double"], ["dotted","dotted"], ["groove","groove"], ["ridge","ridge"], ["inset","inset"], ["outset","outset"], ["inherit","inherit"], ["none","none"], ["hidden","hidden"]]), "borderstyle_");  
   this.appendValueInput("borderwidth_")
