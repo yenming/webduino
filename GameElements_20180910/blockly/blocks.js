9015931e247998b28ef11f1a982d8188e9d7ca91
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-20 19:48:22 +0800

    Add files via upload

--- GameElements_20180910/blockly/blocks.js
+++ GameElements_20180910/blockly/blocks.js
@@ -115,7 +115,7 @@ Blockly.Blocks['table_td_set'] = {
       .appendField(Blockly.Msg.TABLE_TD_Y_SHOW);       
   this.appendDummyInput()  
       .appendField(Blockly.Msg.PROPERTY_SHOW)
-      .appendField(new Blockly.FieldDropdown([["width","width"], ["height","height"], ["background","background"], ["innerHTML","innerHTML"]]), "property_");
+      .appendField(new Blockly.FieldDropdown([["width","width"], ["height","height"], ["textalign","textalign"], ["background","background"], ["innerHTML","innerHTML"]]), "property_");
   this.appendValueInput("value_")
       .setAlign(Blockly.ALIGN_RIGHT)
       .setCheck(null);    
