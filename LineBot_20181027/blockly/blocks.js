946e11d362de7911ea482577a7c64f6bdbb260e7
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-13 22:55:57 +0800

    Update blocks.js

--- LineBot_20181027/blockly/blocks.js
+++ LineBot_20181027/blockly/blocks.js
@@ -132,31 +132,12 @@ Blockly.Blocks['linebot_type'] = {
     //}
   },
   mutationToDom : function() {
-      var container = document.createElement('mutation');
-      container.setAttribute('bot_type', this.getField('value_type').getValue());
-      return container;
+    var container = document.createElement('mutation');
+    container.setAttribute('bot_type', this.getField('value_type').getValue());
+    return container;
   },
   domToMutation : function(xmlElement) {
-    switch (xmlElement.getAttribute('bot_type')) {
-      case 'text':
-          this.getField('value_type').setValue("text");
-          break;
-      case 'sticker':
-          this.getField('value_type').setValue("sticker");
-          break;
-      case 'image':
-          this.getField('value_type').setValue("image");
-          break;   
-      case 'vedio':
-          this.getField('value_type').setValue("vedio");
-          break;  
-      case 'audio':
-          this.getField('value_type').setValue("audio");
-          break;  
-      case 'location':
-          this.getField('value_type').setValue("location");
-          break;          
-    }
+    this.getField('value_type').setValue(xmlElement.getAttribute('bot_type'));
     this.onchange();
   }
 };
@@ -233,22 +214,12 @@ Blockly.Blocks['linenotify_type'] = {
     //}
   },
   mutationToDom : function() {
-      var container = document.createElement('mutation');
-      container.setAttribute('notify_type', this.getField('value_type').getValue());
-      return container;
+    var container = document.createElement('mutation');
+    container.setAttribute('notify_type', this.getField('value_type').getValue());
+    return container;
   },
   domToMutation : function(xmlElement) {
-    switch (xmlElement.getAttribute('notify_type')) {
-      case 'text':
-          this.getField('value_type').setValue("text");
-          break;
-      case 'sticker':
-          this.getField('value_type').setValue("sticker");
-          break;
-      case 'image':
-          this.getField('value_type').setValue("image");
-          break;        
-    }
+    this.getField('value_type').setValue(xmlElement.getAttribute('notify_type'));
     this.onchange();
   }
 };
