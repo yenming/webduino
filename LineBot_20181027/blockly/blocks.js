6015adf89c125da0cdd698cad202b50fb0a122e1
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-13 21:18:22 +0800

    Add files via upload

--- LineBot_20181027/blockly/blocks.js
+++ LineBot_20181027/blockly/blocks.js
@@ -23,27 +23,142 @@ Blockly.Blocks['linebot_notify'] = {
 Blockly.Blocks['linebot_type'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Message (Line Bot)");    
-    this.appendDummyInput()
-        .appendField("Type")
-        .appendField(new Blockly.FieldDropdown([["text (text)","text"], ["sticker (packageId, stickerId)","sticker"], ["image (originalContentUrl, previewImageUrl)","image"], ["video (originalContentUrl, previewImageUrl)","video"], ["audio (originalContentUrl, duration)","audio"], ["location (title, address, latitude, longitude)","location"]]), "value_type");
+        .appendField("Line Bot    ")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(new Blockly.FieldDropdown([["text","text"], ["sticker","sticker"], ["image","image"], ["video","video"], ["audio","audio"], ["location","location"]]), "value_type");
     this.appendValueInput("value_parameter1")
         .setCheck(null)
-        .appendField("Parameter1");
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Parameter1","p1");
     this.appendValueInput("value_parameter2")
         .setCheck(null)
-        .appendField("Parameter2");
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Parameter2","p2");
     this.appendValueInput("value_parameter3")
         .setCheck(null)
-        .appendField("Parameter3");
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Parameter3","p3");
     this.appendValueInput("value_parameter4")
         .setCheck(null)
-        .appendField("Parameter4"); 
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Parameter4","p4"); 
     this.setOutput(true, null);  
     this.setColour(300);
     this.setTooltip("");
     this.setHelpUrl("");
-  }  
+  },
+  onchange: function(event) {
+    //if (event.type=="change") {
+      if (this.getField('value_type').getValue() == 'text') {
+        this.getField('p1').setValue("text");  
+        this.getField('p2').setValue("");
+        this.getField('p3').setValue("");
+        this.getField('p4').setValue("");
+        this.getField('p1').setVisible(true);
+        this.getField('p2').setVisible(false);
+        this.getField('p3').setVisible(false);
+        this.getField('p4').setVisible(false);
+        this.getInput('value_parameter1').setVisible(true);
+        this.getInput('value_parameter2').setVisible(false);
+        this.getInput('value_parameter3').setVisible(false);
+        this.getInput('value_parameter4').setVisible(false);
+      } else if (this.getField('value_type').getValue() == "sticker") {
+        this.getField('p1').setValue("packageId");
+        this.getField('p2').setValue("stickerId");  
+        this.getField('p3').setValue("");
+        this.getField('p4').setValue(""); 
+        this.getField('p1').setVisible(true);
+        this.getField('p2').setVisible(true);
+        this.getField('p3').setVisible(false);
+        this.getField('p4').setVisible(false);
+        this.getInput('value_parameter1').setVisible(true);
+        this.getInput('value_parameter2').setVisible(true);
+        this.getInput('value_parameter3').setVisible(false);
+        this.getInput('value_parameter4').setVisible(false);
+      } else if (this.getField('value_type').getValue() == "image") {
+        this.getField('p1').setValue("originalContentUrl");
+        this.getField('p2').setValue("previewImageUrl"); 
+        this.getField('p3').setValue("");
+        this.getField('p4').setValue(""); 
+        this.getField('p1').setVisible(true);
+        this.getField('p2').setVisible(true);
+        this.getField('p3').setVisible(false);
+        this.getField('p4').setVisible(false);
+        this.getInput('value_parameter1').setVisible(true);
+        this.getInput('value_parameter2').setVisible(true);
+        this.getInput('value_parameter3').setVisible(false);
+        this.getInput('value_parameter4').setVisible(false);
+      } else if (this.getField('value_type').getValue() == "vedio") {
+        this.getField('p1').setValue("(originalContentUrl");
+        this.getField('p2').setValue("previewImageUrl");  
+        this.getField('p3').setValue("");
+        this.getField('p4').setValue(""); 
+        this.getField('p1').setVisible(true);
+        this.getField('p2').setVisible(true);
+        this.getField('p3').setVisible(false);
+        this.getField('p4').setVisible(false);
+        this.getInput('value_parameter1').setVisible(true);
+        this.getInput('value_parameter2').setVisible(true);
+        this.getInput('value_parameter3').setVisible(false);
+        this.getInput('value_parameter4').setVisible(false);
+      } else if (this.getField('value_type').getValue() == "audio") {
+        this.getField('p1').setValue("originalContentUrl");
+        this.getField('p2').setValue("duration");    
+        this.getField('p3').setValue("");
+        this.getField('p4').setValue(""); 
+        this.getField('p1').setVisible(true);
+        this.getField('p2').setVisible(true);
+        this.getField('p3').setVisible(false);
+        this.getField('p4').setVisible(false);
+        this.getInput('value_parameter1').setVisible(true);
+        this.getInput('value_parameter2').setVisible(true);
+        this.getInput('value_parameter3').setVisible(false);
+        this.getInput('value_parameter4').setVisible(false);
+      } else if (this.getField('value_type').getValue() == "location") {
+        this.getField('p1').setValue("title");
+        this.getField('p2').setValue("address");        
+        this.getField('p3').setValue("latitude");  
+        this.getField('p4').setValue("longitude");  
+        this.getField('p1').setVisible(true);
+        this.getField('p2').setVisible(true);
+        this.getField('p3').setVisible(true);
+        this.getField('p4').setVisible(true);
+        this.getInput('value_parameter1').setVisible(true);
+        this.getInput('value_parameter2').setVisible(true);
+        this.getInput('value_parameter3').setVisible(true);
+        this.getInput('value_parameter4').setVisible(true);
+      }
+      this.setOutput(true, null);
+    //}
+  },
+  mutationToDom : function() {
+      var container = document.createElement('mutation');
+      container.setAttribute('bot_type', this.getField('value_type').getValue());
+      return container;
+  },
+  domToMutation : function(xmlElement) {
+    switch (xmlElement.getAttribute('bot_type')) {
+      case 'text':
+          this.getField('value_type').setValue("text");
+          break;
+      case 'sticker':
+          this.getField('value_type').setValue("sticker");
+          break;
+      case 'image':
+          this.getField('value_type').setValue("image");
+          break;   
+      case 'vedio':
+          this.getField('value_type').setValue("vedio");
+          break;  
+      case 'audio':
+          this.getField('value_type').setValue("audio");
+          break;  
+      case 'location':
+          this.getField('value_type').setValue("location");
+          break;          
+    }
+    this.onchange();
+  }
 };
 
 Blockly.Blocks['linenotify'] = {
@@ -67,22 +182,73 @@ Blockly.Blocks['linenotify'] = {
 Blockly.Blocks['linenotify_type'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Message (Line Notify)");    
-    this.appendDummyInput()
-        .appendField("Type")
-        .appendField(new Blockly.FieldDropdown([["text (text)","text"], ["sticker (packageId, stickerId, text)","sticker"], ["image (originalContentUrl, previewImageUrl, text)","image"]]), "value_type");
+        .appendField("Line Notify    ")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(new Blockly.FieldDropdown([["text","text"], ["sticker","sticker"], ["image","image"]]), "value_type");
     this.appendValueInput("value_parameter1")
         .setCheck(null)
-        .appendField("Parameter1");
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Parameter1","p1");
     this.appendValueInput("value_parameter2")
         .setCheck(null)
-        .appendField("Parameter2");
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Parameter2","p2");
     this.appendValueInput("value_parameter3")
         .setCheck(null)
-        .appendField("Parameter3");
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Parameter3","p3");
     this.setOutput(true, null);  
     this.setColour(300);
     this.setTooltip("");
     this.setHelpUrl("");
-  }  
+  },
+  onchange: function(event) {
+    //if (event.type=="change") {
+      if (this.getField('value_type').getValue() == 'text') {
+        this.getField('p1').setValue("text");
+        this.getField('p2').setValue("");
+        this.getField('p3').setValue("");
+        this.getField('p2').setVisible(false);
+        this.getField('p3').setVisible(false);
+        this.getInput('value_parameter2').setVisible(false);
+        this.getInput('value_parameter3').setVisible(false);
+      } else if (this.getField('value_type').getValue() == "sticker") {
+        this.getField('p1').setValue("text");
+        this.getField('p2').setValue("packageId");
+        this.getField('p3').setValue("stickerId");
+        this.getField('p2').setVisible(true);
+        this.getField('p3').setVisible(true);
+        this.getInput('value_parameter2').setVisible(true);
+        this.getInput('value_parameter3').setVisible(true);
+      } else if (this.getField('value_type').getValue() == "image") {
+        this.getField('p1').setValue("text");
+        this.getField('p2').setValue("originalContentUrl");
+        this.getField('p3').setValue("previewImageUrl");
+        this.getField('p2').setVisible(true);
+        this.getField('p3').setVisible(true);
+        this.getInput('value_parameter2').setVisible(true);
+        this.getInput('value_parameter3').setVisible(true);
+      }
+      this.setOutput(true, null);
+    //}
+  },
+  mutationToDom : function() {
+      var container = document.createElement('mutation');
+      container.setAttribute('notify_type', this.getField('value_type').getValue());
+      return container;
+  },
+  domToMutation : function(xmlElement) {
+    switch (xmlElement.getAttribute('notify_type')) {
+      case 'text':
+          this.getField('value_type').setValue("text");
+          break;
+      case 'sticker':
+          this.getField('value_type').setValue("sticker");
+          break;
+      case 'image':
+          this.getField('value_type').setValue("image");
+          break;        
+    }
+    this.onchange();
+  }
 };
